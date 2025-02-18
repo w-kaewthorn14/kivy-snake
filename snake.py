4bd004d7f0d9aad28236ef018285c2f3ff547112
WEERAPHAT_KAEWTHORN <6710110391@psu.ac.th>
2025-02-18 23:11:08 +0700

    Update Implement keyboard controls

--- snake.py
+++ snake.py
@@ -12,9 +12,7 @@ class SnakeGame(Widget):
         
         self.snake = [(10, 10), (9, 10), (8, 10)]  # snake head + body
         self.snake_direction = (1, 0)  # Initial direction (right)
-        self.apple = (random.randint(0, 19), random.randint(0, 19))  # Random apple position
-        self.score = 0
-        
+
         Clock.schedule_interval(self.update, 0.1)  # Update the game every 0.1 second
         Window.bind(on_key_down=self.on_key_down)
 
@@ -35,19 +33,6 @@ class SnakeGame(Widget):
     def update(self, dt):
         new_head = (self.snake[0][0] + self.snake_direction[0], self.snake[0][1] + self.snake_direction[1])
         self.snake = [new_head] + self.snake[:-1]  # Move the snake forward
-        
-        self.canvas.clear()  # Clear previous frame
-        self.snake_color = Color(0, 1, 0)  # Set snake color to green
-        for segment in self.snake:
-            x, y = segment
-            with self.canvas:
-                Rectangle(pos=(x * 20, y * 20), size=(20, 20))
-
-        # Draw the apple
-        self.apple_color = Color(1, 0, 0)  # Set apple color to red
-        x, y = self.apple
-        with self.canvas:
-            Rectangle(pos=(x * 20, y * 20), size=(20, 20))
 
 class SnakeApp(App):
     def build(self):
